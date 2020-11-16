7279a581a07011a2ff87554392f80243f791e9d1
s0567271 <adib.waluya@student.htw-berlin.de>
2020-11-16 18:12:46 +0100

    created functionality for correct and incorrect answers

--- CCA_Two_BrainTrainer/app/src/main/java/de/htwberlin/braintrainer/MainActivity.java
+++ CCA_Two_BrainTrainer/app/src/main/java/de/htwberlin/braintrainer/MainActivity.java
@@ -5,17 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button goButton;
+    TextView sumTextView;
+    ArrayList<Integer> answers = new ArrayList<Integer>();      // Hold the potential answers
+
     public void start(View view) {
         goButton.setVisibility(View.INVISIBLE);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         goButton = findViewById(R.id.goButton);
+
+        sumTextView = findViewById(R.id.sumTextView);
+
+        Random rand = new Random();
+
+        // Picking 21 random different numbers
+        // the lowest being 0, the highest being 20
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        int correctAnswer = rand.nextInt(4);
+        for (int i = 0; i < 4; i++) {
+            if (i == correctAnswer){
+                answers.add(a + b);
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+                while (wrongAnswer == a + b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+                // Added random int between 0 to 40
+                answers.add(wrongAnswer);
+            }
+        }
     }
 }
